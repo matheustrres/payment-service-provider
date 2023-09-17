d2c42351ea0e2d64994777378c59c1bf295c1b0f
Matheus <torresmiranda.rj@gmail.com>
2023-09-17 20:09:32 -0300

    feat: create Transaction entity

---
+++ src/modules/transaction/transaction-entity.ts
@@ -0,0 +1,47 @@
+import { type Card } from './card/card';
+
+import { BaseEntity, type BaseProps } from '@core/base-entity';
+import { type Optional } from '@core/helpers/optional';
+
+export type TransactionProps = BaseProps & {
+	userId: string;
+	payableId?: string;
+	value: number;
+	paymentMethod: string;
+	description?: string;
+	card: Card;
+};
+
+export type OptionalTransactionProps = Optional<TransactionProps, 'createdAt'>;
+
+export class Transaction extends BaseEntity<TransactionProps> {
+	public constructor(props: OptionalTransactionProps, id?: string) {
+		super(
+			{
+				...props,
+				createdAt: props.createdAt ?? new Date(),
+			},
+			id,
+		);
+	}
+
+	public get userId(): string {
+		return this.props.userId;
+	}
+
+	public get payableId(): string | undefined {
+		return this.props.payableId;
+	}
+
+	public get value(): number {
+		return this.props.value;
+	}
+
+	public get description(): string | undefined {
+		return this.props.description;
+	}
+
+	public get card(): Card {
+		return this.props.card;
+	}
+}
