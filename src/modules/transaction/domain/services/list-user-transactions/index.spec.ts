33b8461664f3d5e3eed0c7b2033141eb09e1c52b
Matheus <torresmiranda.rj@gmail.com>
2023-09-18 12:01:20 -0300

    test: ensure SUT returns all user transactions

--- src/modules/transaction/domain/services/list-user-transactions/index.spec.ts
+++ src/modules/transaction/domain/services/list-user-transactions/index.spec.ts
@@ -3,6 +3,7 @@ import { type MockProxy, mock } from 'jest-mock-extended';
 import { type ListUserTransactionsRepository } from '@modules/transaction/data/repositories/list-user-transactions-repository';
 import { type FindUserByIdRepository } from '@modules/user/data/repositories/find-by-id-repository';
 
+import { makeTransaction } from '@tests/factories/transaction';
 import { makeUser } from '@tests/factories/user';
 
 import { ListUserTransactionsService } from '.';
@@ -18,7 +19,16 @@ describe('ListUserTransactions service', (): void => {
 
 		userRepository.findUserById
 			.mockResolvedValueOnce(null)
-			.mockResolvedValueOnce(makeUser({}, 'random_user_id'));
+			.mockResolvedValue(makeUser({}, 'random_user_id'));
+
+		transactionRepository.listUserTransactions
+			.mockResolvedValueOnce([])
+			.mockResolvedValueOnce([
+				makeTransaction(),
+				makeTransaction(),
+				makeTransaction(),
+				makeTransaction(),
+			]);
 	});
 
 	beforeEach((): void => {
@@ -52,5 +62,20 @@ describe('ListUserTransactions service', (): void => {
 		);
 
 		expect(transactions.length).toBe(0);
+	});
+
+	it('should return all user transactions', async (): Promise<void> => {
+		const { transactions } = await sut.exec({ userId: 'random_user_id' });
+
+		expect(userRepository.findUserById).toHaveBeenNthCalledWith(
+			3,
+			'random_user_id',
+		);
+		expect(transactionRepository.listUserTransactions).toHaveBeenNthCalledWith(
+			1,
+			'random_user_id',
+		);
+
+		expect(transactions.length).toBe(4);
 	});
 });
