6dbf41022f8c725cf1faee880369714a285b83a6
Matheus <torresmiranda.rj@gmail.com>
2023-09-19 15:20:52 -0300

    feat: create TransactionViewModel to shape routes responses

--- src/modules/transaction/infra/http/transaction.controller.ts
+++ src/modules/transaction/infra/http/transaction.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
+import {
+	TransactionViewModel,
+	type TransactionToHTTP,
+} from './transaction.view-model';
 
 import { CreateTransactionService } from '@modules/transaction/domain/services/create-transaction';
 
@@ -11,7 +15,11 @@ export class TransactionController {
 	) {}
 
 	@Post()
-	public async createTransactionRoute(@Body() body: CreateTransactionDto) {
-		return this.createTransactionService.exec(body);
+	public async createTransactionRoute(
+		@Body() body: CreateTransactionDto,
+	): Promise<TransactionToHTTP> {
+		const { transaction } = await this.createTransactionService.exec(body);
+
+		return TransactionViewModel.toHTTP(transaction);
 	}
 }
---
+++ src/modules/transaction/infra/http/transaction.view-model.ts
@@ -0,0 +1,39 @@
+import { type Transaction } from '@modules/transaction/domain/entities/transaction-entity';
+
+export type TransactionToHTTP = {
+	id: string;
+	user_id: string;
+	payable_id?: string;
+	value: string;
+	description?: string;
+	card_cvv: string;
+	card_expiration_date: Date;
+	card_owner_name: string;
+	card_number: string;
+	created_at: Date;
+	updated_at?: Date;
+};
+
+export class TransactionViewModel {
+	public static toHTTP(transaction: Transaction): TransactionToHTTP {
+		return {
+			id: transaction.id,
+			user_id: transaction.userId,
+			...(transaction.payableId && {
+				payable_id: transaction.payableId,
+			}),
+			value: transaction.value,
+			...(transaction.description && {
+				description: transaction.description,
+			}),
+			card_cvv: transaction.cardCVV,
+			card_expiration_date: transaction.cardExpirationDate,
+			card_owner_name: transaction.cardOwnerName,
+			card_number: transaction.cardNumber,
+			created_at: transaction.createdAt,
+			...(transaction.updatedAt && {
+				updated_at: transaction.updatedAt,
+			}),
+		};
+	}
+}
