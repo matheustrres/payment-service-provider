b02d5f7041ae42480048b8e23ef110f4b8f26a13
Matheus <torresmiranda.rj@gmail.com>
2023-09-19 11:00:25 -0300

    test: ensure SUT creates a new user

--- src/modules/user/domain/services/create-user/index.spec.ts
+++ src/modules/user/domain/services/create-user/index.spec.ts
@@ -18,11 +18,13 @@ describe('CreateUser service', (): void => {
 	beforeAll((): void => {
 		userRepository = mock();
 
-		userRepository.findUserByEmail.mockResolvedValueOnce(
-			makeUser({
-				email: 'john@example.com',
-			}),
-		);
+		userRepository.findUserByEmail
+			.mockResolvedValueOnce(
+				makeUser({
+					email: 'john@example.com',
+				}),
+			)
+			.mockResolvedValueOnce(null);
 	});
 
 	beforeEach((): void => {
@@ -39,5 +41,28 @@ describe('CreateUser service', (): void => {
 		).rejects.toThrowError(
 			'The email "john@example.com" is already being used.',
 		);
+
+		expect(userRepository.findUserByEmail).toHaveBeenNthCalledWith(
+			1,
+			'john@example.com',
+		);
+	});
+
+	it('should create a new user', async (): Promise<void> => {
+		const { user } = await sut.exec({
+			name: 'Adam Smith',
+			email: 'adam.smith@live.com',
+			password: '*97&CaDF#4%',
+		});
+
+		expect(userRepository.findUserByEmail).toHaveBeenNthCalledWith(
+			2,
+			'adam.smith@live.com',
+		);
+		expect(userRepository.create).toHaveBeenNthCalledWith(1, user);
+
+		expect(user.id).toBeDefined();
+		expect(user.email).toBe('adam.smith@live.com');
+		expect(user.transactions?.length).toBe(0);
 	});
 });
