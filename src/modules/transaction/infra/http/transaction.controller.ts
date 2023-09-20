bf9136cb3dec33a299aeadfd6cae3218b75b2ebe
Matheus <torresmiranda.rj@gmail.com>
2023-09-20 14:12:07 -0300

    feat: add route for listing user transactions

--- src/modules/transaction/infra/http/transaction.controller.ts
+++ src/modules/transaction/infra/http/transaction.controller.ts
@@ -1,25 +1,50 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	ParseUUIDPipe,
+	Post,
+	Query,
+	Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
-import {
-	TransactionViewModel,
-	type TransactionToJSON,
-} from './transaction.view-model';
+import { TransactionViewModel } from './transaction.view-model';
 
 import { CreateTransactionService } from '@modules/transaction/domain/services/create-transaction';
+import { ListUserTransactionsService } from '@modules/transaction/domain/services/list-user-transactions';
 
 @Controller('transactions')
 export class TransactionController {
 	public constructor(
 		private readonly createTransactionService: CreateTransactionService,
+		private readonly listUserTransactionsService: ListUserTransactionsService,
 	) {}
 
 	@Post()
 	public async createTransactionRoute(
 		@Body() body: CreateTransactionDto,
-	): Promise<TransactionToJSON> {
+		@Res() response: Response,
+	) {
 		const { transaction } = await this.createTransactionService.exec(body);
 
-		return TransactionViewModel.toJSON(transaction);
+		return response.send({
+			transaction: TransactionViewModel.toJSON(transaction),
+		});
+	}
+
+	@Get()
+	public async listUserTransactionsRoute(
+		@Query('user_id', new ParseUUIDPipe()) userId: string,
+		@Res() response: Response,
+	) {
+		const { transactions } = await this.listUserTransactionsService.exec({
+			userId,
+		});
+
+		return response.send({
+			transactions: transactions.map(TransactionViewModel.toJSON),
+		});
 	}
 }
--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -3,10 +3,19 @@ import { Module } from '@nestjs/common';
 import { CreateTransactionRepository } from './data/repositories/create-transaction-repository';
 import { ListUserTransactionsRepository } from './data/repositories/list-user-transactions-repository';
 import { CreateTransactionService } from './domain/services/create-transaction';
+import { ListUserTransactionsService } from './domain/services/list-user-transactions';
 import { PgTransactionRepository } from './infra/database/transaction-repository';
 import { TransactionController } from './infra/http/transaction.controller';
 
+import { FindUserByIdRepository } from '@modules/user/data/repositories';
+import { UserModule } from '@modules/user/user.module';
+
+type ListUserTransactionsServiceTransactionRepository =
+	ListUserTransactionsRepository;
+type ListUserTransactionsServiceUserRepository = FindUserByIdRepository;
+
 @Module({
+	imports: [UserModule],
 	providers: [
 		{
 			provide: CreateTransactionRepository,
@@ -21,6 +30,15 @@ import { TransactionController } from './infra/http/transaction.controller';
 			useFactory: (transactionRepository: CreateTransactionRepository) =>
 				new CreateTransactionService(transactionRepository),
 			inject: [CreateTransactionRepository],
+		},
+		{
+			provide: ListUserTransactionsService,
+			useFactory: (
+				transactionRepository: ListUserTransactionsServiceTransactionRepository,
+				userRepository: ListUserTransactionsServiceUserRepository,
+			) =>
+				new ListUserTransactionsService(transactionRepository, userRepository),
+			inject: [ListUserTransactionsRepository, FindUserByIdRepository],
 		},
 	],
 	controllers: [TransactionController],
