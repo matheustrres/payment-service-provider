869c2d08f46212d5e4af9458932b3b4f6d7d59df
Matheus <torresmiranda.rj@gmail.com>
2023-09-22 20:38:01 -0300

    test: ensure SUT throws if user has no payables

--- src/modules/payable/domain/services/list-user-payables/index.spec.ts
+++ src/modules/payable/domain/services/list-user-payables/index.spec.ts
@@ -3,6 +3,8 @@ import { type MockProxy, mock } from 'jest-mock-extended';
 import { type ListUserPayablesRepository } from '@modules/payable/data/repositories';
 import { type FindUserByIdRepository } from '@modules/user/data/repositories';
 
+import { makeUser } from '@tests/factories/user';
+
 import { ListUserPayablesService } from '.';
 
 describe('ListUserPayables service', (): void => {
@@ -15,6 +17,13 @@ describe('ListUserPayables service', (): void => {
 		userRepository = mock();
 
 		userRepository.findUserById.mockResolvedValueOnce(null);
+		userRepository.findUserById.mockResolvedValue(
+			makeUser({
+				id: 'random_user_id',
+			}),
+		);
+
+		payableRepository.listUserPayables.mockResolvedValueOnce(null);
 	});
 
 	beforeEach((): void => {
@@ -27,5 +36,25 @@ describe('ListUserPayables service', (): void => {
 				userId: 'fake_user_id',
 			}),
 		).rejects.toThrowError('No user were found with ID "fake_user_id".');
+
+		expect(userRepository.findUserById).toHaveBeenNthCalledWith(
+			1,
+			'fake_user_id',
+		);
+		expect(payableRepository.listUserPayables).not.toHaveBeenCalled();
+	});
+
+	it('should throw if user has no payables', async (): Promise<void> => {
+		await expect(
+			sut.exec({
+				userId: 'random_user_id',
+			}),
+		).rejects.toThrowError('No payables were found.');
+
+		expect(userRepository.findUserById).toHaveBeenNthCalledWith(
+			2,
+			'random_user_id',
+		);
+		expect(payableRepository.listUserPayables).toHaveBeenCalledTimes(1);
 	});
 });
