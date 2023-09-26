c18a4ebc59b373bbaa481bd278eab8b5f1fe5f54
Matheus <torresmiranda.rj@gmail.com>
2023-09-26 19:52:09 -0300

    test: ensure SUT throws if given password doesn't match user password

--- src/modules/user/domain/services/login-user/index.spec.ts
+++ src/modules/user/domain/services/login-user/index.spec.ts
@@ -4,6 +4,8 @@ import { type CompareStrings } from '@core/contracts/hashing';
 
 import { type FindUserByEmailRepository } from '@modules/user/data/repositories';
 
+import { makeUser } from '@tests/factories/user';
+
 import { LoginUserService } from '.';
 
 describe('LoginUser service', (): void => {
@@ -15,7 +17,16 @@ describe('LoginUser service', (): void => {
 		userRepository = mock();
 		hashService = mock();
 
-		userRepository.findUserByEmail.mockResolvedValueOnce(null);
+		userRepository.findUserByEmail
+			.mockResolvedValueOnce(null)
+			.mockResolvedValueOnce(
+				makeUser({
+					email: 'john.doe@gmail.com',
+					password: 'this_is_a_hashed_password_string',
+				}),
+			);
+
+		hashService.compareStrings.mockResolvedValueOnce(false);
 	});
 
 	beforeEach((): void => {
@@ -35,5 +46,23 @@ describe('LoginUser service', (): void => {
 			'fake_email@gmail.com',
 		);
 		expect(hashService.compareStrings).not.toHaveBeenCalled();
+	});
+
+	it('should throw if given password does not match user password', async (): Promise<void> => {
+		await expect(
+			sut.exec({
+				email: 'john.doe@gmail.com',
+				password: 'random_password',
+			}),
+		).rejects.toThrowError('Invalid credentials.');
+
+		expect(userRepository.findUserByEmail).toHaveBeenNthCalledWith(
+			2,
+			'john.doe@gmail.com',
+		);
+		expect(hashService.compareStrings).toHaveBeenNthCalledWith(1, {
+			plainText: 'random_password',
+			hash: 'this_is_a_hashed_password_string',
+		});
 	});
 });
