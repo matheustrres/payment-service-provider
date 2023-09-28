522fd58aa78e000e4d2a745b27ea9b029cafa8c7
Matheus <torresmiranda.rj@gmail.com>
2023-09-28 10:12:39 -0300

    fix: import HttpModule

--- src/ioC/app.module.ts
+++ src/ioC/app.module.ts
@@ -3,11 +3,12 @@ import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 import { DatabaseModule } from '@infra/database/database.module';
+import { HttpModule } from '@infra/http/http.module';
 
 import { TransactionModule } from '@modules/transaction/transaction.module';
 
 @Module({
-	imports: [DatabaseModule, TransactionModule],
+	imports: [DatabaseModule, HttpModule, TransactionModule],
 	providers: [
 		{
 			provide: APP_PIPE,
