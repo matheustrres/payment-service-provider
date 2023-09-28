8d4777d6ce30d77a05a675aa8b03a6dbb1e88729
Matheus <torresmiranda.rj@gmail.com>
2023-09-28 10:29:16 -0300

    feat: use LocalAuthGuard on login route & fetch logged in user

--- src/modules/user/infra/http/user.controller.ts
+++ src/modules/user/infra/http/user.controller.ts
@@ -1,23 +1,28 @@
-import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Post,
+	Res,
+	UseGuards,
+	UseInterceptors,
+} from '@nestjs/common';
 import { Response } from 'express';
 
 import { CreateUserDto } from './dtos';
-import { LoginUserDto } from './dtos/login-user.dto';
 import { type UserToJSON, UserViewModel } from './user.view-model';
 
 import { type ApiHttpResponse } from '@types';
 
+import { LoggedInUser } from '@infra/http/authentication/decorators/logged-user.decorator';
+import { LocalAuthGuard } from '@infra/http/authentication/guards/local-auth.guard';
 import { JwtTokenInterceptor } from '@infra/http/authentication/interceptors/jwt-token.interceptor';
 
+import { User } from '@modules/user/domain/entities/user-entity';
 import { CreateUserService } from '@modules/user/domain/services/create-user';
-import { LoginUserService } from '@modules/user/domain/services/login-user';
 
 @Controller('users')
 export class UserController {
-	public constructor(
-		private readonly createUserService: CreateUserService,
-		private readonly loginUserService: LoginUserService,
-	) {}
+	public constructor(private readonly createUserService: CreateUserService) {}
 
 	@Post()
 	public async createUserRoute(
@@ -32,10 +37,9 @@ export class UserController {
 	}
 
 	@Post('login')
+	@UseGuards(LocalAuthGuard)
 	@UseInterceptors(JwtTokenInterceptor)
-	public async loginUserRoute(@Body() body: LoginUserDto): Promise<UserToJSON> {
-		const { user } = await this.loginUserService.exec(body);
-
+	public async loginUserRoute(@LoggedInUser() user: User): Promise<UserToJSON> {
 		return UserViewModel.toJSON(user);
 	}
 }
