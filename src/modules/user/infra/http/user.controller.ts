97488456b396db7296b98982ebf47471ea82f30a
Matheus <torresmiranda.rj@gmail.com>
2023-10-08 15:46:23 -0300

    feat: inject loginUserService for authentication

--- src/modules/user/infra/http/user.controller.ts
+++ src/modules/user/infra/http/user.controller.ts
@@ -8,19 +8,24 @@ import {
 } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos';
+import { LoginUserDto } from './dtos/login-user.dto';
 import { type UserToJSON, UserViewModel } from './user.view-model';
 
 import { LoggedInUser } from '@infra/http/authentication/decorators/logged-user.decorator';
 import { JwtAuthGuard } from '@infra/http/authentication/guards/jwt-auth.guard';
 import { LocalAuthGuard } from '@infra/http/authentication/guards/local-auth.guard';
-import { JwtTokenInterceptor } from '@infra/http/authentication/interceptors/jwt-token.interceptor';
+import { JwtInterceptor } from '@infra/http/authentication/interceptors/jwt.interceptor';
 
 import { User } from '@modules/user/domain/entities/user-entity';
 import { CreateUserService } from '@modules/user/domain/services/create-user';
+import { LoginUserService } from '@modules/user/domain/services/login-user';
 
 @Controller('users')
 export class UserController {
-	public constructor(private readonly createUserService: CreateUserService) {}
+	public constructor(
+		private readonly createUserService: CreateUserService,
+		private readonly loginUserService: LoginUserService,
+	) {}
 
 	@Post()
 	public async createUserRoute(
@@ -33,9 +38,16 @@ export class UserController {
 
 	@Post('login')
 	@UseGuards(LocalAuthGuard)
-	@UseInterceptors(JwtTokenInterceptor)
-	public async loginUserRoute(@LoggedInUser() user: User): Promise<UserToJSON> {
-		return UserViewModel.toJSON(user);
+	@UseInterceptors(JwtInterceptor)
+	public async loginUserRoute(
+		@Body() body: LoginUserDto,
+	): Promise<LoginUserResponse> {
+		const { accessToken, user } = await this.loginUserService.exec(body);
+
+		return {
+			user: UserViewModel.toJSON(user),
+			access_token: accessToken,
+		};
 	}
 
 	@Get('me')
@@ -44,3 +56,8 @@ export class UserController {
 		return UserViewModel.toJSON(user, true, true);
 	}
 }
+
+type LoginUserResponse = {
+	user: UserToJSON;
+	access_token: string;
+};
